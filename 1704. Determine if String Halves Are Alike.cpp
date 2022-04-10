a1c44f865dfe0057d800520265b48fdf2aecb7d1
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 14:31:31 +0530

    Create 1704. Determine if String Halves Are Alike.cpp

---
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int prev=0;
+        int next=0;
+        
+        for(int i=0;i<s.length()/2;i++)
+        {
+            if(s[i]=='A'||s[i]=='a'||s[i]=='E'||s[i]=='e'||s[i]=='I'||s[i]=='i'||s[i]=='O'||s[i]=='o'||s[i]=='U'||s[i]=='u')
+                prev++;
+        }
+        
+        for(int i=s.length()/2;i<s.length();i++)
+        {
+            if(s[i]=='A'||s[i]=='a'||s[i]=='E'||s[i]=='e'||s[i]=='I'||s[i]=='i'||s[i]=='O'||s[i]=='o'||s[i]=='U'||s[i]=='u')
+                next++;
+        }
+        
+        if(next==prev)
+            return true;
+        
+        else
+            return false;
+    }
+};
