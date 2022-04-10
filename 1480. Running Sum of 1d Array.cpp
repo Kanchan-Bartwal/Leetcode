5878ef7e165611667ccad1cd780828ebbe7ad81e
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 14:16:39 +0530

    Create 1480. Running Sum of 1d Array.cpp

---
+++ 1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int result[]=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length; i++)
+        {
+            sum=sum+nums[i];
+            result[i]=sum;
+        }
+        return result;
+    }
+}
