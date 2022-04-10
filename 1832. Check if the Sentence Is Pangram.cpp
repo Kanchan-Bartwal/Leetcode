96efc07f26e163cf4b32c6a45bfaa688e81262cb
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 15:07:06 +0530

    Create 1832. Check if the Sentence Is Pangram.cpp

---
+++ 1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        int arr[26]={0};
+        
+        for(int i=0;i<s.length();i++)
+        {
+            arr[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]==0)
+                return false;
+        }
+        return true;
+    }
+};
