1f8671770c694e29b7cfed040369488093ed68ca
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 13:32:42 +0530

    Create 283. Move Zeroes.cpp

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j]!=0)
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        for(int k=i;k<nums.size();k++)
+        {
+            nums[k]=0;
+        }
+    }
+};
